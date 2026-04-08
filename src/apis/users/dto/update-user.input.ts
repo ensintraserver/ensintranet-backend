28dd371fd387c650d34974edab02072407d4d478
JoonkiKim <qkjk@DESKTOP-LH9FVAI>
2026-04-08 15:44:13 +0900

    권한 기본 값 덮어쓰기해결

--- src/apis/users/dto/update-user.input.ts
+++ src/apis/users/dto/update-user.input.ts
@@ -1,11 +1,16 @@
 // users/dto/update-user.input.ts
 import { InputType, PartialType, Field } from '@nestjs/graphql';
-import { IsString, IsOptional, MinLength } from 'class-validator';
+import { IsString, IsOptional } from 'class-validator';
 import { CreateUserInput } from './create-user.input';
 import { CreateCareerInput } from 'src/apis/career/dto/create-career.input';
+import { UserRole } from '../entities/user.entity';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
+  @Field(() => UserRole, { nullable: true })
+  @IsOptional()
+  role?: UserRole;
+
   @Field(() => String, { nullable: true })
   @IsString()
   @IsOptional()
